f5011e9e549ab39acf3820cf41e325c06a9965c2
DPearson2445 <danpearson2445@gmail.com>
2025-02-24 10:56:20 -0500

    "Created test_export_report"

---
+++ test_export_report.py
@@ -0,0 +1,48 @@
+# test_export_report.py
+from selenium import webdriver
+from src.pages.login_page import LoginPage
+from src.pages.home_page import HomePage
+from locators.locators import *
+import time
+import pytest
+import os
+
+
+
+@pytest.mark.selenium
+def test_export_report():
+
+
+    browsers = ["Chrome", "Edge", "Firefox"]
+    for browser in browsers:
+        if browser == "Chrome":
+            driver = webdriver.Chrome()
+        elif browser == "Edge":
+            driver = webdriver.Edge()
+        elif browser == "Firefox":
+            driver = webdriver.Firefox()
+        print("Running Test in: " + browser)
+        driver.get(BasePageLocators.BASE_URL)
+        time.sleep(3)
+        login_page = LoginPage(driver)
+        login_page.verify_page_http_200_response(LoginPageLocators.URL)
+        time.sleep(2)
+        login_page.login()
+        time.sleep(3)
+        home_page = HomePage(driver)
+        time.sleep(2)
+        home_page.click_export_report()
+        time.sleep(5)
+        file_path = r"C:\Users\daniel.pearson\Downloads\export (30)" # Raw string to handle backslashes in Windows paths
+
+        if os.path.exists(file_path):
+            print(f"File exists at: {file_path}")
+            if os.path.isfile(file_path):
+                print("This is a file")
+            else:
+                print("This is not a file.")
+        else:
+            print(f"File does not exist at: {file_path}")
+        print("###########################################################")
+        # Close the WebDriver
+        driver.quit()
