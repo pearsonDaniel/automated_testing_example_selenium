9e7b9d2da1a994bc3cc6e98e0121dd1f14d9d3e1
DPearson2445 <danpearson2445@gmail.com>
2025-03-11 08:41:51 -0400

    Added the confest.py file to git.

---
+++ conftest.py
@@ -0,0 +1,33 @@
+# config.py
+from selenium import webdriver
+import os
+import platform
+
+def config_browser(browser_name):
+        if browser_name == "Chrome":
+            options = webdriver.ChromeOptions()
+            print("Session User: " + str(os.getlogin()))
+            print("OS Name: " + str(platform.system()))
+            print("OS Version: " + str(platform.version()))
+            print("Browser: " + browser_name + str())
+            return webdriver.Chrome(options=options)
+        elif browser_name == "Edge":
+            options = webdriver.EdgeOptions()
+            print("Session User: " + str(os.getlogin()))
+            print("OS Name: " + str(platform.system()))
+            print("OS Version: " + str(platform.version()))
+            print("Browser: " + browser_name + str())
+            return webdriver.Edge(options=options)
+        elif browser_name == "Firefox":
+            options = webdriver.FirefoxOptions()
+            print("Session User: " + str(os.getlogin()))
+            print("OS Name: " + str(platform.system()))
+            print("OS Version: " + str(platform.version()))
+            print("Browser: " + browser_name + str())
+            return webdriver.Firefox(options=options)
+        else:
+            raise ValueError(f"Browser '{browser_name}' is not supported.")
+        
+
+class Config:
+    BASE_URL = 'https://mtt-staging.cldigitalservices.com/'
