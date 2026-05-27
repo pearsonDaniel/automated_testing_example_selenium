c8f813ab56c4c39d6bc93b50f44faa045c68dd39
Pearson <daniel.pearson@cloudlakellc.com>
2026-05-27 12:20:50 -0400

    Changed directory name for home page tests.

--- src/pages/home_page.py
+++ src/pages/home_page.py
@@ -8,6 +8,13 @@ class HomePage(BasePage):
     def __init__(self, driver):
         self.driver = driver
 
+
+    # Functions to Add and Remove items from cart, with verification of button text changes to confirm action was successful
+
+    # Accepts two locators as arguments - the first is the locator for the "Add to cart" button, 
+    # and the second is the locator for the "Remove" button for the same item. 
+    # The function clicks the "Add to cart" button, then waits for the button text to 
+    # change to "Remove" to verify that the item was added to the cart successfully.
     def add_item(self, add_item_locator, remove_item_locator):
         print("Attempting to add item to cart...")
         WebDriverWait(self.driver, 10).until(
@@ -19,6 +26,10 @@ class HomePage(BasePage):
         assert self.driver.find_element(*remove_item_locator).text == "Remove"
         print("***PASS: Item added to cart successfully***")
 
+    # Accepts two locators as arguments - the first is the locator for the "Remove" button, 
+    # and the second is the locator for the "Add to cart" button for the same item.
+    # The function clicks the "Remove" button, then waits for the button text to 
+    # change to "Add to cart" to verify that the item was removed from the cart successfully.
     def remove_item(self, remove_item_locator, add_item_locator):
         print("Attempting to remove item from cart...")
         WebDriverWait(self.driver, 10).until(
